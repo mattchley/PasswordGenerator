bae1fe9527568f89763f1bebbeb353d33cbcfc9f
mattchley <mattchley@gmail.com>
2019-10-01 14:02:58 -0700

    utilizedFunciton

--- security.js
+++ security.js
@@ -1,3 +1,4 @@
+// Generate random [password]
 function generate( length = 12 ){
     var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     var lowercase = 'abcdefghijklmnopqrstuvwxyz';
@@ -5,9 +6,13 @@ function generate( length = 12 ){
     var symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
     var all = uppercase + lowercase + numbers + symbols;
     var password = '';
-    for (var index = 0; index < length; index++) {
+    for (var i = 0; i < length; i++) {
         var character = Math.floor(Math.random() * all.length);
         password += all.substring(character, character + 1);
     }
-    return password;
+    document.getElementById("password").value = password; 
+   
+
 }
+
+// return password;
