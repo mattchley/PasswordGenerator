c84072b3fe694665f0ec3b1e0c705ba4f2262885
mattchley <mattchley@gmail.com>
2019-10-02 10:32:57 -0700

    addedCharacterLimit

--- security.js
+++ security.js
@@ -1,9 +1,11 @@
-// Makes the password length parameter
-function random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+
+// needs to have alerts to confirm for lower, upper, numbers, symbols 
 // Makes the password
-function generate( length = random(8, 128) ){
+function generate(length = prompt("how many characters between 8-128?")) {
+    var uCase = prompt("Would you like to use Uppercase? Y/N");
+    var lCase = prompt("Would you like to use Lowercase? Y/N");
+    var num = prompt("Would you like to use Numbers? Y/N");
+    var symbols = prompt("Would you like to use Symbols? Y/N");
     var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     var lowercase = 'abcdefghijklmnopqrstuvwxyz';
     var numbers = '0123456789';
@@ -14,6 +16,6 @@ function generate( length = random(8, 128) ){
         var character = Math.floor(Math.random() * all.length);
         password += all.substring(character, character + 1);
     }
-    document.getElementById("password").value = password; 
+    document.getElementById("password").value = password;
 }
 // Copies the password text to Clipboard
